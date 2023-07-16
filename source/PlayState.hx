d5c6b5c7056c9a2ab6c5b036821351180bd9aa1c
Itz_MIles <95124554+Itz-Miles@users.noreply.github.com>
2023-07-15 21:59:46 -0600

    vc (playstate)

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -296,32 +296,6 @@ class PlayState extends MusicBeatState
 
 		switch (curStage)
 		{
-			case 'stage': // Week 1
-				var bg:BGSprite = new BGSprite('stageback', -600, -200, 0.9, 0.9);
-				add(bg);
-
-				var stageFront:BGSprite = new BGSprite('stagefront', -650, 600, 0.9, 0.9);
-				stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
-				stageFront.updateHitbox();
-				add(stageFront);
-				if (!ClientPrefs.lowQuality)
-				{
-					var stageLight:BGSprite = new BGSprite('stage_light', -125, -100, 0.9, 0.9);
-					stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
-					stageLight.updateHitbox();
-					add(stageLight);
-					var stageLight:BGSprite = new BGSprite('stage_light', 1225, -100, 0.9, 0.9);
-					stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
-					stageLight.updateHitbox();
-					stageLight.flipX = true;
-					add(stageLight);
-
-					var stageCurtains:BGSprite = new BGSprite('stagecurtains', -500, -300, 1.3, 1.3);
-					stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
-					stageCurtains.updateHitbox();
-					add(stageCurtains);
-				}
-
 			case 'general', 'shesmad':
 				var generalStageBack:FlxSprite = new FlxSprite(-600, -300, Paths.image("vcback", "freeplay"));
 				var generalStageFront:FlxSprite = new FlxSprite(-650, 600, Paths.image("vcfront", "freeplay"));
@@ -333,6 +307,13 @@ class PlayState extends MusicBeatState
 				var spaceStageFront:FlxSprite = new FlxSprite(0, 429, Paths.image("earth", "freeplay"));
 				add(spaceStageBack);
 				add(spaceStageFront);
+
+			case 'vc':
+				var vcBack:FlxSprite = new FlxSprite(0, 0, Paths.image("vcback", "freeplay"));
+				add(vcBack);
+
+				var vcFront:FlxSprite = new FlxSprite(0, 538, Paths.image("vcfront", "freeplay"));
+				add(vcFront);
 		}
 
 		add(gfGroup);
